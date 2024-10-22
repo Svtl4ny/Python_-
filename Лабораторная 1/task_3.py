c748860a0cddeb362a6d7649dcca8f2b524fb844
Svtl4ny <matthewsvetlichniy@gmail.com>
2024-10-22 15:19:09 +0300

    Update task_3.py

--- Лабораторная 1/task_3.py
+++ Лабораторная 1/task_3.py
@@ -1,5 +1,11 @@
-disc_ = 1.44*1024**2
-book_size_ = 4*25*50*100
-number_of_books_ = round(disc_//book_size_)
-
-print("Количество книг, помещающихся на дискету:", number_of_books_)
+floppy_Mb = 1.44
+Mb_to_Kb = 1024
+Kb_to_B = 1024
+page_n = 100
+str_n = 50
+symb_n = 25
+symb_size_B = 4
+floppy_B = floppy_Mb*Mb_to_Kb*Kb_to_B
+book_size_ = symb_size_B*symb_n*str_n*page_n
+number_of_books_ = floppy_B/book_size_
+print("Количество книг, помещающихся на дискету:", int(number_of_books_))
