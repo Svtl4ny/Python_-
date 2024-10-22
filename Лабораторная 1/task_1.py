0e6302d75b12a320ebf696ab7fb49ba32bea0820
Svtl4ny <matthewsvetlichniy@gmail.com>
2024-10-22 15:18:23 +0300

    Update task_1.py

--- Лабораторная 1/task_1.py
+++ Лабораторная 1/task_1.py
@@ -1,8 +1,10 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-numbers[4] = 0
+num_ = numbers.index(None)
+if numbers.index(None):
+    numbers[num_] = 0
 sum_ = sum(numbers)
 kol_ = len(numbers)
 none_ = sum_/kol_
-numbers[4] = none_
+numbers[num_] = none_
 
 print("Измененный список:", numbers)
